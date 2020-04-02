c66ced07153b20e4521b10a82eb89e001a82b73a
Graham Bewley <grahambewley@gmail.com>
2020-04-01 23:01:20 -0400

    Try new CORS method

--- app.js
+++ app.js
@@ -9,6 +9,21 @@ const { determineOutcome } = require('./utils/gameLogic');
 const app = express();
 const port = process.env.PORT || 4001;
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+
+app.use(allowCrossDomain);
 app.use(cors());
 app.use(express.json());
 app.use(index);
