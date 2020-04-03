65ffddae48e6c32560094a68b400ebe8af007171
Graham Bewley <grahambewley@gmail.com>
2020-04-02 20:28:54 -0400

    Change cors middleware options

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const cors = require('cors');
 
 const http = require("http");
 const socketIo = require("socket.io");
@@ -10,62 +9,20 @@ const { findUserInGame, updateGameStatus } = require('./utils/gameDatabase');
 const app = express();
 const port = process.env.PORT || 4001;
 
-app.use(cors());
+app.use(
+  cors({
+      credentials: true,
+      origin: true
+  })
+);
+app.options('*', cors());
+
 app.use(express.json());
 app.use(index);
 
 const server = http.createServer(app);
 
 const io = socketIo(server);
-
-// const gameStatus = {
-//   name: 'Trash Palace Waterfall',
-//   turnIndex: 0,
-//   players: [
-//     {
-//       name: 'Graham',
-//       qMaster: false,
-//       tMaster: false
-//     },
-//     {
-//       name: 'Jordan',
-//       qMaster: false,
-//       tMaster: false
-//     },{
-//       name: 'John',
-//       qMaster: false,
-//       tMaster: false
-//     },
-//     {
-//       name: 'Joe',
-//       qMaster: false,
-//       tMaster: false
-//     }
-//   ],
-//   rules: [
-//     // 'If you touch your face, you drink'
-//   ],
-//   dates: [
-
-//   ],
-//   unplayedCards: [
-//     '2H', '2D', '2C', '2S',
-//     '3H', '3D', '3C', '3S',
-//     '4H', '4D', '4C', '4S',
-//     '5H', '5D', '5C', '5S',
-//     '6H', '6D', '6C', '6S',
-//     '7H', '7D', '7C', '7S',
-//     '8H', '8D', '8C', '8S',
-//     '9H', '9D', '9C', '9S',
-//     '10H', '10D', '10C', '10S',
-//     'JH', 'JD', 'JC', 'JS',
-//     'QH', 'QD', 'QC', 'QS',
-//     'KH', 'KD', 'KC', 'KS',
-//     'AH', 'AD', 'AC', 'AS'
-//   ],
-//   lastPulledCard: '',
-//   lastPulledCardOutcome: ''
-// }
 
 io.on("connection", socket => {
   // When a new client connects, log to console and emit them the current gameStatus
